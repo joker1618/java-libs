c58b4d51a696e9fc3a0ce1dc9e3d6d10d74699cb
federicobarbano@gmail.com <F3d3r!C0pq2aui1618>
2018-09-13 23:55:13 +0200

    audio info new

--- src/main/java/xxx/joker/libs/javalibs/media/analysis/JkMediaAnalyzer.java
+++ src/main/java/xxx/joker/libs/javalibs/media/analysis/JkMediaAnalyzer.java
@@ -3,7 +3,13 @@ package xxx.joker.libs.javalibs.media.analysis;
 import it.sauronsoftware.jave.Encoder;
 import it.sauronsoftware.jave.EncoderException;
 import it.sauronsoftware.jave.MultimediaInfo;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.mp4.MP4Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import xxx.joker.libs.javalibs.utils.JkConverter;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,17 +19,30 @@ import java.nio.file.Path;
  */
 public class JkMediaAnalyzer {
 
-	public static JkVideoInfo analyzeVideo(Path videoPath) throws EncoderException, FileNotFoundException {
-		if(!Files.exists(videoPath))	throw new FileNotFoundException(videoPath + " not found");
-		Encoder encoder = new Encoder();
-		MultimediaInfo info = encoder.getInfo(videoPath.toFile());
-		return new JkVideoInfo(videoPath, info);
-	}
+    public static JkVideoInfo analyzeVideo(Path videoPath) throws Exception {
+        if (!Files.exists(videoPath)) throw new FileNotFoundException(videoPath + " not found");
 
-	public static JkAudioInfo analyzeAudio(Path audioPath) throws EncoderException, FileNotFoundException {
-		if(!Files.exists(audioPath))	throw new FileNotFoundException(audioPath + " not found");
-		Encoder encoder = new Encoder();
-		MultimediaInfo info = encoder.getInfo(audioPath.toFile());
-		return new JkAudioInfo(audioPath, info);
-	}
+        try (FileInputStream inputstream = new FileInputStream(videoPath.toFile())) {
+            BodyContentHandler handler = new BodyContentHandler();
+            Metadata metadata = new Metadata();
+            ParseContext pcontext = new ParseContext();
+
+            MP4Parser MP4Parser = new MP4Parser();
+            MP4Parser.parse(inputstream, handler, metadata, pcontext);
+
+            JkVideoInfo jkVideoInfo = new JkVideoInfo();
+            jkVideoInfo.setVideoPath(videoPath);
+            jkVideoInfo.setContentType(metadata.get("Content-Type"));
+
+            long dur = Math.round(JkConverter.stringToDouble(metadata.get("xmpDM:duration"))*1000d);
+            jkVideoInfo.setDuration(dur);
+
+            jkVideoInfo.setWidth(JkConverter.stringToInteger(metadata.get("tiff:ImageWidth")));
+            jkVideoInfo.setHeight(JkConverter.stringToInteger(metadata.get("tiff:ImageLength")));
+            jkVideoInfo.setSamplingRate(JkConverter.stringToInteger(metadata.get("xmpDM:audioSampleRate")));
+
+            return jkVideoInfo;
+        }
+    }
+
 }
--- src/main/java/xxx/joker/libs/javalibs/media/analysis/JkVideoInfo.java
+++ src/main/java/xxx/joker/libs/javalibs/media/analysis/JkVideoInfo.java
@@ -15,73 +15,61 @@ public class JkVideoInfo {
 	private Path videoPath;
 	// Multimedia info
 	private long duration;	// in millis
-	private String format;
-	// Video specific info
+	private String contentType;
 	private int width;
 	private int height;
-	private int bitRate;
-	private float frameRate;
-	private String decoder;
+	private int samplingRate;
 	
 	
-	JkVideoInfo(Path videoPath, MultimediaInfo mInfo) {
-		this.videoPath = videoPath;
-
-		this.duration = mInfo.getDuration();
-		this.format = mInfo.getFormat();
-
-		VideoInfo video = mInfo.getVideo();
-		this.width = video.getSize().getWidth();
-		this.height = video.getSize().getHeight();
-		this.bitRate = video.getBitRate();
-		this.frameRate = video.getFrameRate();
-		this.decoder = video.getDecoder();
+	protected JkVideoInfo() {
+	    
 	}
 
+    public Path getVideoPath() {
+        return videoPath;
+    }
 
-	@Override
-	public String toString() {
-		StringBuilder sbfields = new StringBuilder();
-		sbfields.append("Path:|").append(videoPath.toAbsolutePath()).append("\n");
-		sbfields.append("Resolution:|").append(width).append(" x ").append(height).append("\n");
-		sbfields.append("Duration:|").append(duration).append("\n");
-		sbfields.append("Format:|").append(format).append("\n");
-		sbfields.append("Bit rate:|").append(bitRate).append("\n");
-		sbfields.append("Frame rate:|").append(frameRate).append("\n");
-		sbfields.append("Decoder:|").append(decoder).append("\n");
-		String fieldStr = new JkColumnFmtBuilder().addLines(sbfields.toString()).toString("|", 2);
-		fieldStr = JkStrings.leftPadLines(fieldStr, " ", 3);
+    public long getDuration() {
+        return duration;
+    }
 
+    public String getContentType() {
+        return contentType;
+    }
 
-		StringBuilder sb = new StringBuilder();
-		sb.append("Video file \"").append(videoPath.getFileName()).append("\" ");
-		sb.append("{\n");
-		sb.append(fieldStr);
-		sb.append("}");
+    public int getWidth() {
+        return width;
+    }
 
-		return sb.toString();
-	}
+    public int getHeight() {
+        return height;
+    }
 
+    public int getSamplingRate() {
+        return samplingRate;
+    }
 
-	public int getWidth() {
-		return width;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public long getDuration() {
-		return duration;
-	}
-	public String getFormat() {
-		return format;
-	}
-	public int getBitRate() {
-		return bitRate;
-	}
-	public float getFrameRate() {
-		return frameRate;
-	}
-	public String getDecoder() {
-		return decoder;
-	}
+    protected void setVideoPath(Path videoPath) {
+        this.videoPath = videoPath;
+    }
+
+    protected void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    protected void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+
+    protected void setSamplingRate(int samplingRate) {
+        this.samplingRate = samplingRate;
+    }
 }
