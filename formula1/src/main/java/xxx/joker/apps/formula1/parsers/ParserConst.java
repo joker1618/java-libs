592eccf68df1f43d5ae311507485b18edcf9bbe5
joker1618 <federicobarbano@gmail.com>
2019-04-09 00:36:38 +0100

    fix

--- formula1/src/main/java/xxx/joker/apps/formula1/parsers/ParserConst.java
+++
@@ -1,7 +0,0 @@
-package xxx.joker.apps.formula1.parsers;
-
-class ParserConst {
-
-    public static final String LINK_DRIVER_PREFIX = "driver.";
-    public static final String LINK_FLAG_PREFIX = "driver.";
-}
