f6f0cc9f914918ebe35b04aa1736f88d5df90c90
joker1618 <federicobarbano@gmail.com>
2019-09-28 00:19:08 +0100

    fix

--- java-repo/src/main/java/xxx/joker/libs/datalayer/jpa/JpaHandler.java
+++ java-repo/src/main/java/xxx/joker/libs/datalayer/jpa/JpaHandler.java
@@ -175,7 +175,7 @@ public class JpaHandler {
         return JkStreams.toMapSingle(proxies.entrySet(), e -> (Class<T>)e.getKey(), e -> (Set<T>) e.getValue().getProxyDataSet());
     }
     public <T extends RepoEntity> Set<T> getDataSet(Class<T> entityClazz) {
-        if(proxies.containsKey(entityClazz)) {
+        if(!proxies.containsKey(entityClazz)) {
             throw new JkRuntimeException("Unable to get data set of type {}: class not managed (loaded classes = {})", entityClazz, proxies.keySet());
         }
         return (Set<T>) proxies.get(entityClazz).getProxyDataSet();
